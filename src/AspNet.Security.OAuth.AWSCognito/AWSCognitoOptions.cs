779e5d2a000bb5ef127999c8d0af01145635877a
Chris Christou <biochristou@users.noreply.github.com>
2017-08-23 20:27:51 -0600

    Added Claim Mappings for the defined Cognito Claims

--- src/AspNet.Security.OAuth.AWSCognito/AWSCognitoOptions.cs
+++ src/AspNet.Security.OAuth.AWSCognito/AWSCognitoOptions.cs
@@ -14,14 +14,34 @@ namespace AspNet.Security.OAuth.AWSCognito
 		public RegionEndpoint AmazonRegionEndpoint { get; set; } = RegionEndpoint.USEast1;
 
 		// todo : automate AuthorizationEndpoint and TokenEndpoint by taking UserPoolDomainPrefix and assembling the urls
+		// https://XXXXXXXXXXX.auth.REGIONLOWERCASE.amazoncognito.com
 
 		public AWSCognitoOptions()
 		{
 			Scope.Add(AWSCognitoScopes.AwsCognitoSigninUserAdmin);
 
-			ClaimActions.MapJsonKey(ClaimTypes.Name, "name");
-			ClaimActions.MapJsonKey(ClaimTypes.Surname, "family_name");
+			// AWS Cognito claims
+			// http://docs.aws.amazon.com/cognito/latest/developerguide/user-pool-settings-attributes.html
+			ClaimActions.MapJsonKey(ClaimTypes.StreetAddress, "address");
+			ClaimActions.MapJsonKey(ClaimTypes.DateOfBirth, "birthdate");
 			ClaimActions.MapJsonKey(ClaimTypes.Email, "email");
+			ClaimActions.MapJsonKey(ClaimTypes.Surname, "family_name");
+			ClaimActions.MapJsonKey(ClaimTypes.Gender, "gender");
+			ClaimActions.MapJsonKey(ClaimTypes.GivenName, "given_name");
+			ClaimActions.MapJsonKey(ClaimTypes.Locality, "locale");
+			ClaimActions.MapJsonKey("MiddleName", "middle_name");
+			ClaimActions.MapJsonKey(ClaimTypes.Name, "name");
+			ClaimActions.MapJsonKey("Nickname", "nickname");
+			ClaimActions.MapJsonKey(ClaimTypes.HomePhone, "phone_number");
+			ClaimActions.MapJsonKey("Picture", "picture");
+			ClaimActions.MapJsonKey("PreferredName", "preferred_username");
+			ClaimActions.MapJsonKey("Profile", "profile");
+			ClaimActions.MapJsonKey("Timezone", "timezone");
+			ClaimActions.MapJsonKey("Timezone", "timezone");
+			ClaimActions.MapJsonKey("UpdatedAt", "updated_at");
+			ClaimActions.MapJsonKey(ClaimTypes.Webpage, "website");
+
+			// UserId
 			ClaimActions.MapCustomJson(ClaimTypes.NameIdentifier, (json) =>
 			{
 				JToken token;
